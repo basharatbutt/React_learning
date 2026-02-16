5ac67d8d5dfe8476bf1f0037152a9cd0e4f22646
basharatbutt <basharatdev57@gmail.com>
2026-02-16 11:32:30 +0500

    Update App.jsx

--- Bg-changer/src/App.jsx
+++ Bg-changer/src/App.jsx
@@ -2,60 +2,33 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  // 1. Initialize state with a Tailwind color class (e.g., 'bg-pink-500')
-  const [color, setColor] = useState("bg-pink-300")
+const [Color, setColor] = useState("pink")
 
   return (
-    // 2. Use the 'color' variable inside the className using template literals {}
-    <div className={`w-full h-screen ${color} flex items-center justify-center`}>
-      <div className="flex gap-12">
-        <button 
-          onClick={() => setColor("bg-red-500")}
-          className="px-4 py-2 bg-red-500 text-white rounded-lg"
-        >
-          Red
-        </button>
-
-        <button 
-          onClick={() => setColor("bg-blue-500")}
-          className="px-4 py-2 bg-blue-500 text-white rounded-lg"
-        >
-          Blue
-        </button>
-
-        <button 
-          onClick={() => setColor("bg-green-500")}
-          className="px-4 py-2 bg-green-500 text-white rounded-lg"
-        >
-          Green
-        </button>
-        <button 
-          onClick={() => setColor("bg-orange-500")}
-          className="px-4 py-2 bg-orange-500 text-white rounded-lg"
-        >
-          Orange
-        </button>
-        <button 
-          onClick={() => setColor("bg-yellow-500")}
-          className="px-4 py-2 bg-yellow-500 text-white rounded-lg"
-        >
-          Yellow
-        </button>
-        <button 
-          onClick={() => setColor("bg-white")}
-          className="px-4 py-2 bg-white text-black rounded-lg"
-        >
-          White
-        </button>
-        <button 
-          onClick={() => setColor("bg-stone-500")}
-          className="px-4 py-2 bg-stone-500 text-white rounded-lg"
-        >
-          Brown
-        </button>
-      </div>
-      </div>
-
+<>
+<div className='w-full h-screen'
+style={{backgroundColor: Color}}>
+  <div className='flex justify-center gap-4'>
+    <button onClick={() =>  setColor (" red")}
+       className="px-4 py-2 bg-red-500"
+        style={{backgroundColor: "red"}}>
+      red
+    </button>
+    <button onClick={() =>  setColor (" blue")}
+       className="px-4 py-2 bg-blue-500" >
+      blue
+    </button>
+    <button onClick={() =>  setColor (" green")}
+       className="px-4 py-2 bg-green-500" >
+      green
+    </button>
+    <button onClick={() =>  setColor (" yellow")}
+       className="px-4 py-2 bg-yellow-500" >
+      yellow
+    </button>
+  </div>
+</div>
+</>
   )
 }
 
