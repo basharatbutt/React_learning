d3c6e8147a820a980d002cf9c15d65b10384e5d9
basharatbutt <basharatdev57@gmail.com>
2026-02-16 17:41:22 +0500

    d

--- React 5 Project 1 using useEffect,useRef and useCallbasck/Project 1/src/App.jsx
+++ React 5 Project 1 using useEffect,useRef and useCallbasck/Project 1/src/App.jsx
@@ -27,7 +27,6 @@ useEffect(()=>{
 },[length, allowedchar, allowednum, passwordGenerator])
 
 const copypass = useCallback(()=>{
-  passwordRef.current?.select();
   window.navigator.clipboard.writeText(password)
 },[password])
 
@@ -45,6 +44,15 @@ const copypass = useCallback(()=>{
           readOnly
           ref={passwordRef}
         />
+         <button 
+          onClick={passwordGenerator}
+          className='outline-none bg-blue-500 text-white px-3 hover:bg-blue-600 transition-colors duration-200 border-r border-blue-400'
+          title="Generate New Password"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+          </svg>
+        </button>
         <button 
         onClick={copypass}
           className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 hover:bg-blue-600 transition-colors duration-200 font-medium'
