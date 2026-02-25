067483ab35edc5a0cf88e62aa4b777ea95516048
basharatbutt <basharatdev57@gmail.com>
2026-02-25 15:55:34 +0500

    l

---
+++ node.js/index.js
@@ -0,0 +1,2 @@
+const biti = require("./second");
+console.log("hello world", biti)
---
+++ node.js/second.js
@@ -0,0 +1,7 @@
+let bash = {
+    name: "basharat",
+    age:20,
+    developer : true,
+}
+
+module.exports = bash;
