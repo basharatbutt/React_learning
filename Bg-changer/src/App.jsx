73963b0e22819984fde2d538005d38278e33f7c5
basharatbutt <basharatdev57@gmail.com>
2026-03-04 12:09:15 +0500

    Update App.jsx

--- Bg-changer/src/App.jsx
+++ Bg-changer/src/App.jsx
@@ -1,36 +1,40 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
-const [Color, setColor] = useState("pink")
-
+const [Color, setColor] = useState("brown")
   return (
-<>
-<div className='w-full h-screen'
-style={{backgroundColor: Color}}>
-  <div className='flex justify-center gap-4'>
-    <button onClick={() =>  setColor (" red")}
-       className="px-4 py-2 bg-red-500"
-        style={{backgroundColor: "red"}}>
-      red
-    </button>
-    <button onClick={() =>  setColor (" blue")}
-       className="px-4 py-2 bg-blue-500" >
-      blue
-    </button>
-    <button onClick={() =>  setColor (" green")}
-       className="px-4 py-2 bg-green-500" >
-      green
-    </button>
-    <button onClick={() =>  setColor (" yellow")}
-       className="px-4 py-2 bg-yellow-500" >
-      yellow
-    </button>
-  </div>
-</div>
-</>
+   <div className="scr w-full h-screen" style={{background : Color}}>
+    
+    <div className="flex justify-center fixed bottom-12 inset-x-0 flex-wrap">
+     <div className='w-150 bg-gray-950 rounded-3xl flex justify-center py-2 gap-3'>
+
+      <button 
+      className='w-22 bg-blue-500 rounded-2xl px-4 py-1 '
+      onClick={() => setColor ("blue")}>
+         bule
+      </button>
+
+      <button 
+      className='w-22 bg-red-500 rounded-2xl px-4 py-1 '
+      onClick={() => setColor ("red")}>
+         red
+      </button>
+
+      <button 
+      className='w-22 bg-green-500 rounded-2xl px-4 py-1 '
+      onClick={() => setColor ("green")}>
+         green
+      </button>
+      <button 
+      className='w-22 bg-yellow-500 rounded-2xl px-4 py-1 '
+      onClick={() => setColor ("yellow")}>
+         yellow
+      </button>
+      </div>
+     </div>
+
+   </div>
   )
 }
 
 export default App
-
