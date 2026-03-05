c4af4274b1eafbb37fd8310bec1b03554666a850
basharatbutt <basharatdev57@gmail.com>
2026-03-05 12:19:54 +0500

    tiktak

---
+++ React project tiktoe/Tik-Tak-Toe/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  <svg class="svg-icon" style="width: 1em; height: 1em;vertical-align: middle;fill: currentColor;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M810.65984 170.65984q18.3296 0 30.49472 12.16512t12.16512 30.49472q0 18.00192-12.32896 30.33088l-268.67712 268.32896 268.67712 268.32896q12.32896 12.32896 12.32896 30.33088 0 18.3296-12.16512 30.49472t-30.49472 12.16512q-18.00192 0-30.33088-12.32896l-268.32896-268.67712-268.32896 268.67712q-12.32896 12.32896-30.33088 12.32896-18.3296 0-30.49472-12.16512t-12.16512-30.49472q0-18.00192 12.32896-30.33088l268.67712-268.32896-268.67712-268.32896q-12.32896-12.32896-12.32896-30.33088 0-18.3296 12.16512-30.49472t30.49472-12.16512q18.00192 0 30.33088 12.32896l268.32896 268.67712 268.32896-268.67712q12.32896-12.32896 30.33088-12.32896z"  /></svg>
+
+  return (
+    <>
+    <header>
+      <h1 className='flex text-3xl justify-center py-23  align-middle text-center'>Hey!Welcome to my Tik-Tak-Toe</h1>
+    </header>
+    <main className="pic">
+      <div className="picture flex text-3xl justify-center items-center">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaBIk7DfRBjfDy1E8zY9Z1CtFlEGJ-I57U1Q&s" alt="Tik-Tak-Toe"/>
+      </div>
+    </main>
+    </>
+  )
+}
+
+export default App
