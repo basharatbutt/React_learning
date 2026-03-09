f48dd3231d405233c36e0c12fe18480117901629
basharatbutt <basharatdev57@gmail.com>
2026-03-09 13:03:40 +0500

    cal

--- calculator project in react/Calculator/src/App.jsx
+++ calculator project in react/Calculator/src/App.jsx
@@ -1,33 +1,58 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="mian w-100 h-100">
+       <div className="calculator bg-mist-900 px-22 gap-8">
+
+
+        <div className="input">
+          <input type="text" />
+        </div>
+
+
+<div className="allbtn bg-amber-600 grid  gap-4">
+         <div className="buttons">
+          <input type="button" value="AC" />
+          <input type="button" value="DC" />
+          <input type="button" value="." />
+          <input type="button" value="/" />
+         </div>
+
+         <div className="buttons">
+          <input type="button" value="7" />
+          <input type="button" value="8" />
+          <input type="button" value="9" />
+          <input type="button" value="*" />
+         </div>
+
+         <div className="buttons">
+          <input type="button" value="4" />
+          <input type="button" value="5" />
+          <input type="button" value="6" />
+          <input type="button" value="+" />
+         </div>
+
+         <div className="buttons">
+          <input type="button" value="1" />
+          <input type="button" value="2" />
+          <input type="button" value="3" />
+          <input type="button" value="-" />
+         </div>
+
+         <div className="buttons">
+          <input type="button" value="00" />
+          <input type="button" value="0" />
+          <input type="button" value="=" />
+         </div>
+         </div>
+
+
+       </div>
+    </div>
     </>
   )
 }
