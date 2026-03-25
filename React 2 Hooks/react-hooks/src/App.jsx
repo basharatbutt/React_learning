2f3a3cf92b043f9bca9c29812284892e59ba981d
basharatbutt <basharatdev57@gmail.com>
2026-03-25 11:37:54 +0500

    s

--- React 2 Hooks/react-hooks/src/App.jsx
+++ React 2 Hooks/react-hooks/src/App.jsx
@@ -1,30 +1,14 @@
 import { useState } from 'react'
 
 function App() {
-  const[counter, setCounter] = useState(8)
+  const[counter, setCounter] = useState(5)
 
-  // let counter = 6
-
-   let addValue = () => {
-    
-    if (counter < 20 ) {
-      setCounter( counter + 1)
-    }
-   }
-   let removeValue = () => {
-    
-    if (counter > 0 ) {
-      setCounter( counter - 1)
-    }
-   }
 
   return (
     <>
-      <h1>chai and react</h1>
-      <h2>counter value: {counter}</h2>
+     <div>click to update the counter{counter}</div>
+     <button onClick={() => setCounter(counter + 1)}> click </button>
 
-      <button onClick={addValue}>Add Value</button>
-      <button onClick={removeValue}>Remove Value</button>
     </>
   )
 }
