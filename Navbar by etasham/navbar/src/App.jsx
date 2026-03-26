d69218c1da7add9cf02bdd6ad41eb13dbe89e120
basharatbutt <basharatdev57@gmail.com>
2026-03-26 15:12:05 +0500

    Update App.jsx

--- Navbar by etasham/navbar/src/App.jsx
+++ Navbar by etasham/navbar/src/App.jsx
@@ -6,10 +6,10 @@ function App() {
 
   return (
     <>
-     <div className="flex pt-15 pl-162">
-      <nav className=' text-xl gap-3 bg-amber-700 w-300 h-20'>
-        <ul className='flex gap-10 '>
-          <li>img</li>
+     <div className="flex pt-15 justify-center bg-gray-100 bg-[url('https://www.transparenttextures.com/patterns/cubes.png')] min-h-screen">
+      <nav className=' text-xl gap-3  bg-white/70 backdrop-blur-lg border  shadow-sm   h-20 flex items-center px-5 rounded-lg'>
+        <ul className='flex gap-10 w-full '>
+          <img className='w-32 h-32' src="https://dynamicleo.ae/assets/navbar-logo-BuO6n3VF.png" alt=""  />
           <li>About Us </li>
           <li>Services</li>
           <li>Industries</li>
