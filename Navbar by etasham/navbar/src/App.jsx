714283860a8b003ad0db1c34a28bf89484509df1
basharatbutt <basharatdev57@gmail.com>
2026-03-26 16:03:08 +0500

    Update App.jsx

--- Navbar by etasham/navbar/src/App.jsx
+++ Navbar by etasham/navbar/src/App.jsx
@@ -7,14 +7,18 @@ function App() {
   return (
     <>
      <div className="flex pt-15 justify-center bg-gray-100 bg-[url('https://www.transparenttextures.com/patterns/cubes.png')] min-h-screen">
-      <nav className=' text-xl gap-3  bg-white/70 backdrop-blur-lg border  shadow-sm   h-20 flex items-center px-5 rounded-lg'>
-        <ul className='flex gap-10 w-full '>
-          <img className='w-32 h-32' src="https://dynamicleo.ae/assets/navbar-logo-BuO6n3VF.png" alt=""  />
+      <nav className=' text-xl gap-3  bg-white/70 backdrop-blur-lg border  shadow-sm  w-222 h-17 flex items-center px-5 rounded-full '>
+
+ <img className='w-9 h-9' src="https://dynamicleo.ae/assets/navbar-logo-BuO6n3VF.png" alt=""  />
+
+        <ul className='flex gap-10 w-full items-center justify-around object-contain px-9'>
+         
           <li>About Us </li>
           <li>Services</li>
           <li>Industries</li>
-          <li>Our Work ⌄</li>
-          <li>Contact Us </li>
+          <li className='flex py-1'>Our Work  <span className='flex items-center justify-center '>⌄</span></li>
+          <li className='flex pl-26 bg-blue-50 ' >Contact Us </li>
+
         </ul>
       </nav>
      </div>
